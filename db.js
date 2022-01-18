5ba88befcbeb64a534d76cf74b6ed5dca12aaa0a
Oscar Sanz <osanzce@gmail.com>
2022-01-18 21:17:17 +0100

    FIX: db  connection

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI_TEST, {
+    mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
     console.log('connected to database');
