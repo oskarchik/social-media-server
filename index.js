c33353786a0b886fe2c03277015723fe5cf803ae
Oscar Sanz <osanzce@gmail.com>
2022-01-18 20:16:44 +0100

    FIX: removes https socket cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: '*',
+    origin: '//social-face.netlify.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
