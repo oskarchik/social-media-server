3fa8166df01591284cfe2322196e5fcea5753fc8
Oscar Sanz <osanzce@gmail.com>
2022-01-18 18:15:04 +0100

    FIX: res.header access-control-allow-origin front url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const io = socketio(server, {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://social-face.netlify.app/');
   // res.header('Access-Control-Allow-Headers', ['Content-Type, multipart/form-data']);
   next();
 });
