fd1ba77181dc5c74107abf05e877fb695799fbbc
Oscar Sanz <osanzce@gmail.com>
2022-01-19 19:32:24 +0100

    FIX: secure proxy

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.set('trust proxy', 1);
+app.set('trust proxy', 1);
 app.use(
   session({
     secret: process.env.SECRET_SESSION,
