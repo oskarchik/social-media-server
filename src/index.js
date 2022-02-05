a608173ada1e4cadf50b883b3423a626119cfa2b
Oscar Sanz <osanzce@gmail.com>
2022-02-05 18:03:00 +0100

    REFACTOR: improves file readability

--- src/index.js
+++ src/index.js
@@ -2,93 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-// const morgan = require('morgan');
-// const helmet = require('helmet');
-// const cors = require('cors');
-// const passport = require('passport');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo');
-
 const { connect } = require('./db');
 const sockets = require('./socket/sockets');
 
-// const usersRoute = require('./routes/users');
-// const authRoute = require('./routes/auth');
-// const postsRoute = require('./routes/posts');
-// const commentsRoute = require('./routes/comments');
-// const conversationsRoute = require('./routes/conversations');
-// const messagesRoute = require('./routes/messages');
-
 connect();
-require('./passport/passport');
-
-const PORT = process.env.PORT || 5500;
-// const app = express();
 
 const { app } = require('./app');
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 
-const socketServer = socketIO(server, {
+const PORT = process.env.PORT || 5500;
+
+const socketServer = socketIO(httpServer, {
   cors: {
     origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.FRONT_URL,
     credentials: true,
     methods: ['GET', 'POST'],
   },
 });
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   next();
-// });
-
-// app.use(helmet());
-// app.use(morgan('common'));
-
-// app.use(
-//   cors({
-//     origin: [process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.FRONT_URL],
-//     credentials: true,
-//   })
-// );
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.set('trust proxy', 1);
-// app.use(
-//   session({
-//     secret: process.env.SECRET_SESSION,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 24 * 60 * 60 * 1000,
-//       httpOnly: true,
-//       secure: true,
-//       sameSite: 'none',
-//     },
-//     store: MongoStore.create({ mongoUrl: DB_URL }),
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.use('/api/users', usersRoute);
-// app.use('/api/auth', authRoute);
-// app.use('/api/posts', postsRoute);
-// app.use('/api/comments', commentsRoute);
-// app.use('/api/conversations', conversationsRoute);
-// app.use('/api/messages', messagesRoute);
-
-// app.use('*', (req, res, next) => {
-//   const error = new Error('Route not found');
-//   error.status = 404;
-//   next(error);
-// });
-
-// app.use((err, req, res, next) => {
-//   return res.status(err.status || 500).json({ error: err.message || 'Unexpected error' });
-// });
 
 server.listen(PORT, () => {
   console.log(`running on http://localhost:${PORT}`);
